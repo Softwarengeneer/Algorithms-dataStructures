7a9c98b73c9234424ac57bb92b238528e8d09d23
Softwarengeneer <58634690+Softwarengeneer@users.noreply.github.com>
2021-05-31 21:55:29 +0300

    Create L.cpp
    Рассмотрим последовательность целых чисел длины N. По ней с шагом 1 двигается «окно» длины K, то есть сначала в «окне» видно первые K чисел, на следующем шаге в «окне» уже будут находиться K чисел, начиная со второго, и так далее до конца последовательности. Требуется для каждого положения «окна» определить минимум в нём.
    
    Формат ввода
    В первой строке входных данных содержатся два числа N и K (1 ≤ N ≤ 150000, 1 ≤ K ≤ 10000, K ≤ N) – длины последовательности и «окна», соответственно. На следующей строке находятся N чисел – сама последовательность. Числа последовательности не превосходят по модулю 105.
    
    Формат вывода
    Выходые данные должны содержать N - K + 1 строк – минимумы для каждого положения «окна».

---
+++ L.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int line[3000000];
+int t[1000000];
+void build (int v, int tl, int tr) {
+    if (tl == tr)
+        t[v] = line[tl];
+    else {
+        int tm = (tl + tr) / 2;
+        build (v*2, tl, tm);
+        build (v*2+1, tm+1, tr);
+        t[v] = min(t[v*2] , t[v*2+1]);
+    }
+}
+int get_min(int v, int tl, int tr, int l, int r) {
+    if (l > r)
+        return 2281488;
+    if (l == tl && r == tr)
+        return t[v];
+    int tm = (tl + tr) / 2;
+    return min(get_min(v * 2, tl, tm, l, min(r, tm)), get_min(v * 2 + 1, tm + 1, tr, max(l, tm + 1), r));
+}
+int main()
+{
+    int n;
+    int k;
+    cin >> n;
+    cin >> k;
+    for (int i = 1; i <= n; ++i)
+        cin >> line[i];
+    build(1, 1, n);
+    for(int i = 0; i <= n - k; ++i)
+        cout << get_min(1, 0, n - 1, i, i + k - 1) << ' ';
+    return 0;
+}
