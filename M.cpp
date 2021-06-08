2b67a4cd1c9fd6c28afb2491d4ad84e033a76b59
Softwarengeneer <58634690+Softwarengeneer@users.noreply.github.com>
2021-06-08 03:20:16 +0300

    Create M.cpp
    Карта мира в компьютерной игре «Цивилизация» версии 1 представляет собой прямоугольник, разбитый на квадратики. Каждый квадратик может иметь один из нескольких возможных рельефов, для простоты ограничимся тремя видами рельефов  — поле, лес и вода. Поселенец перемещается по карте, при этом на перемещение в клетку, занятую полем, необходима одна единица времени, на перемещение в лес  — две единицы времени, а перемещаться в клетку с водой нельзя.
    
    У вас есть один поселенец, вы определили место, где нужно построить город, чтобы как можно скорее завладеть всем миром. Найдите маршрут переселенца, приводящий его в место строительства города, требующий минимального времени. На каждом ходе переселенец может перемещаться в клетку, имеющую общую сторону с той клеткой, где он сейчас находится.
    
    Формат ввода
    Во входном файле записаны два натуральных числа N и M, не превосходящих 1000  — размеры карты мира (N  — число строк в карте, M  — число столбцов). Затем заданы координаты начального положения поселенца x и y, где x  — номер строки, y  — номер стролбца на карте (1 ≤ x ≤ N, 1 ≤ y ≤ M), строки нумеруются сверху вниз, столбцы  — слева направо. Затем аналогично задаются координаты клетки, куда необходимо привести поселенца.
    
    Далее идет описание карты мира в виде N строк, каждая из которых содержит M символов. Каждый символ может быть либо «.» (точка), обозначающим поле, либо «W», обозначающим лес, либо «#», обозначающим воду. Гарантируется, что начальная и конечная клетки пути переселенца не являются водой.
    
    Формат вывода
    В первой строке выходного файла выведите количество единиц времени, необходимое для перемещения поселенца (перемещение в клетку с полем занимает 1 единицу времени, перемещение в клетку с лесом  — 2 единицы времени). Во второй строке выходного файла выведите последовательность символов, задающих маршрут переселенца. Каждый символ должен быть одним из четырех следующих: «N» (движение вверх), «E» (движение вправо), «S» (движение вниз), «W» (движение влево). Если таких маршрутов несколько, выведите любой из них.
    
    Если дойти из начальной клетки в конечную невозможно, выведите число -1.

---
+++ M.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <string>
+
+
+using namespace std;
+
+int main()
+{
+    int n, m, xFront, yFront, xBack, yBack;
+    cin >> n >> m >> xFront >> yFront >> xBack >> yBack;
+    vector<vector<int>> count(n, vector<int>(m, INT32_MAX));
+    vector<vector<int>> playground(n, vector<int>(m, -1));
+    
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            char way;
+            cin >> way;
+            if (way == 'W') {
+                playground[i][j] = 2;
+            }
+            if (way == '.') {
+                playground[i][j] = 1;
+            }
+        }
+    }
+
+    queue<pair<int, int>> line;
+    xBack -= 1;
+    yBack -= 1;
+    count[xFront - 1][yFront - 1] = 0;
+    line.push({xFront - 1, yFront - 1});
+    while (!line.empty()) {
+        int x = line.front().first;
+        int y = line.front().second;
+        line.pop();
+        if (x + 1 < n) {
+            if (playground[x + 1][y] != -1) {
+                if (count[x + 1][y] > count[x][y] + playground[x + 1][y]) {
+                    count[x + 1][y] = count[x][y] + playground[x + 1][y];
+                    line.push({x + 1, y });
+                }
+            }
+        }
+        if (x - 1 > -1) {
+            if (playground[x - 1][y] != -1) {
+                if (count[x - 1][y] > count[x][y] + playground[x - 1][y]) {
+                    count[x - 1][y] = count[x][y] + playground[x - 1][y];
+                    line.push({x - 1, y });
+                }
+            }
+        }
+        if (y - 1 > -1) {
+            if (playground[x][y - 1] != -1) {
+                if (count[x][y - 1] > count[x][y] + playground[x][y - 1]) {
+                    count[x][y - 1] = count[x][y] + playground[x][y - 1];
+                    line.push({x, y - 1});
+                }
+            }
+        }
+
+        if (y + 1 < m) {
+            if (playground[x][y + 1] != -1) {
+                if (count[x][y + 1] > count[x][y] + playground[x][y + 1]) {
+                    count[x][y + 1] = count[x][y] + playground[x][y + 1];
+                    line.push({x, y + 1});
+                }
+            }
+        }
+    }
+
+    int x = xBack;
+    int y = yBack;
+    if (count[x][y] == INT32_MAX) {
+        cout << -1;
+        return 0;
+    }
+
+    cout << count[x][y] << endl;
+    string answer;
+    while (count[x][y] != 0) {
+
+
+        if (x + 1 < n && count[x + 1][y] + playground[x][y] == count[x][y]) {
+            answer += "N";
+            ++x;
+            continue;
+        }
+        if (x - 1 > -1 && count[x - 1][y] + playground[x][y] == count[x][y]) {
+            answer += "S";
+            --x;
+            continue;
+        }
+
+        if (y - 1 > -1 && count[x][y - 1] + playground[x][y] == count[x][y]) {
+            answer += "E";
+            --y;
+            continue;
+        }
+
+        if (y + 1 < m && count[x][y + 1] + playground[x][y] == count[x][y]) {
+            answer += "W";
+            ++y;
+            continue;
+        }
+
+    }
+	std::reverse(answer.begin(), answer.end());
+
+    cout << answer;
+
+    return 0;
+}
